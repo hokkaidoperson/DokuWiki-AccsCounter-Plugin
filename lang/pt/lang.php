cf5b7bd7a7a4bb3279eee1415eecf38f6a7da6ff
Paulo Schopf <pschopf@gmail.com>
2020-05-28 17:00:38 +0200

    translation update

---
+++ lang/pt/lang.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Paulo Schopf <pschopf@gmail.com>
+ */
+$lang['selectall']             = 'Marcar todos';
+$lang['pagename']              = 'número da página';
+$lang['sofar']                 = 'PVs até agora';
+$lang['lastdate']              = 'Último acesso em';
+$lang['today']                 = 'PVs de hoje';
+$lang['yest']                  = 'PVs de ontem';
+$lang['ipadd']                 = 'Endereço IP do último visitante';
+$lang['pleasechoose']          = '**ESCOLHER**';
+$lang['sfscheck']              = 'Verificação de Spammer';
+$lang['delete']                = 'Excluir o Log';
+$lang['run']                   = 'Rodar';
+$lang['sfstried']              = 'A página %s foi acessada por um spammer (endereço IP: %s) por %d vezes; portanto, o sistema deduziu o número de acessos do spammer dos PVs até agora e dos PVs de hoje. Por favor, confirme.';
+$lang['sfsfinish']             = 'Terminou a verificação dos spammers.';
+$lang['mngfaileddel']          = 'Erro ao excluir o arquivo de log da página %s. Confirme se você tem permissão para acessar arquivos dentro do diretório que o sistema salva os metafiles. Se você não entender isso, exclua os arquivos você mesmo com ferramentas como um aplicativo FTP.';
+$lang['mngdelfinish']          = 'Terminada a exclusão do log.';
