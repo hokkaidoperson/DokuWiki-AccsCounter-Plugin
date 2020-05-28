c935c1b87efc806bd781aed6dc88b44e6986ae89
Paulo Schopf <pschopf@gmail.com>
2020-05-28 17:05:35 +0200

    translation update

---
+++ lang/pt/settings.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Paulo Schopf <pschopf@gmail.com>
+ */
+$lang['excludeMgAndSp_o_0']    = 'Contar ambos';
+$lang['excludeMgAndSp_o_sp']   = 'Não contar superusuários';
+$lang['excludeMgAndSp_o_mg']   = 'Não contar gerenciadores (incluindo superusuários)';
+$lang['saveLog_o_0']           = 'Não salvar';
+$lang['saveLog_o_ppage']       = 'Salvar (Não criar arquivos para cada data)';
+$lang['saveLog_o_pdate']       = 'Salvar (Criar arquivos para cada data)';
